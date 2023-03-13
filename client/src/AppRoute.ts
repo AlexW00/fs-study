529077fdca9b17af6dead6304323963c73653c98
Alexander Weichart <55558407+AlexW00@users.noreply.github.com>
2023-03-13 15:11:37 +0100

    fix: double pre task

--- client/src/AppRoute.ts
+++ client/src/AppRoute.ts
@@ -11,7 +11,11 @@ export enum AppRoute {
 
 export const getAppRoute = (state: State): AppRoute => {
 	console.log(state.didGiveConsent, state.pairingCode, state.isPaired);
-	if (!state.didGiveConsent && state.pairingCode === "") return AppRoute.Setup;
+	if (
+		(!state.didGiveConsent && state.pairingCode === "") ||
+		state.run.tasks.length === 0
+	)
+		return AppRoute.Setup;
 	else if (state.pairingCode !== "" && !state.isPaired)
 		return AppRoute.Unpaired;
 	else if (!state.didGiveConsent && state.isPaired) return AppRoute.PreStudy;
