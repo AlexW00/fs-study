123020fac42361c1457e2584edf10ab5b28e5f0a
Alexander Weichart <55558407+AlexW00@users.noreply.github.com>
2023-03-15 14:45:56 +0100

    added: post task participant id

--- client/src/views/routes/postStudy.ts
+++ client/src/views/routes/postStudy.ts
@@ -1,12 +1,32 @@
 import { html } from "@arrow-js/core";
+import state from "../../classes/State";
 
 export const $postStudy = html`
 	<div>
 		<h1>Post Task Questionnaire</h1>
-		<p>Bitte füllen Sie noch diesen Fragebogen aus:</p>
-		<a href="https://docs.google.com/forms/d/e/1FAIpQLSfI6I48fHAMztpd9KuqbA2FdskhxniM683db1U3CyH5jjNAnw/viewform?usp=sf_link">
+		<span class="end-text">
+			Ihre Teilnehmer-ID ist:
+			<span id="participant-id">${() => state.pairingCode}</span>
+		</span>
+		<br />
+		<span class="end-text">
+			Bitte kopieren Sie Ihre Teilnehmer-ID und füllen Sie den folgenden
+			Fragebogen aus.
+		</span>
+		<span class="end-text">
+			Sie müssen den Fragebogen nur <b>EINMAL</b> ausfüllen, das Gerät (Desktop
+			oder Mobil) ist dabei egal.
+		</span>
+		<span class="end-text">
+			Nach dem Ausfüllen des Fragebogens werden Sie weitergeleitet, um die
+			Versuchspersonenstunden (VP) erhalten zu können.
+		</span>
+		<a
+			target="_blank"
+			rel="noopener noreferrer"
+			href="https://docs.google.com/forms/d/e/1FAIpQLSfI6I48fHAMztpd9KuqbA2FdskhxniM683db1U3CyH5jjNAnw/viewform?usp=sf_link"
+		>
 			Zum Fragebogen
 		</a>
-		<p>Nach dem Ausfüllen des Fragebogens werden Sie weitergeleitet, um die Versuchspersonenstunden (VP) erhalten zu können.</p>
 	</div>
 `;
