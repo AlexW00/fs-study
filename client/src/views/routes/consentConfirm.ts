63390b32204c6984bc9f6b7bd187571aee2497ba
Alexander Weichart <55558407+AlexW00@users.noreply.github.com>
2023-03-20 22:13:21 +0100

    debug

--- client/src/views/routes/consentConfirm.ts
+++ client/src/views/routes/consentConfirm.ts
@@ -3,46 +3,15 @@ import { SocketEvent } from "../../../../shared/SocketEvent";
 import { setDidGiveConsent } from "../../classes/State";
 import { SocketManager } from "../../Socket";
 
-const didCheckConsent = reactive({
-	value: false,
-});
+const onConsent = () => {
+    setDidGiveConsent(true);
+    SocketManager.getInstance().emit(SocketEvent.SendGiveConsent, undefined);
+}
 
-const doShowCheckConsentError = reactive({
-	value: false,
-});
-
-const onCheckConsent = () => {
-	console.log("onCheckConsent");
-	didCheckConsent.value = !didCheckConsent.value;
-	if (didCheckConsent.value) {
-		doShowCheckConsentError.value = false;
-	}
-};
-
-const onClickConsent = () => {
-	console.log("onClickConsent");
-	if (!didCheckConsent.value) {
-		doShowCheckConsentError.value = true;
-	} else {
-		doShowCheckConsentError.value = false;
-		setDidGiveConsent(true);
-		SocketManager.getInstance().emit(SocketEvent.SendGiveConsent, undefined);
-	}
-};
-
-const $checkConsentError = html`
-	<div id="check-consent-error">
-		Bitte bestätigen Sie, dass Sie die Einverständniserklärung zur Aufklärung
-		über die Teilnahme gelesen habe^n.
+export const $instructions = html`
+	<div>
+		<h1>Erklärungddd der Studie</h1>
+		
+		<button @click="${onConsent}">Verstanden</button>
 	</div>
-`;
-export const $consentConfirm = html`
-<div class="consent-check" >
-    <input type="checkbox" name="Einverständniserklärung" @input="${onCheckConsent} value="${didCheckConsent.value}"/>
-    <label for="0">Ich habe die Einverständniserklärung zur Aufklärung über die Teilnahme sorgfältig durchgelesen und stimme dieser zu.</label>
-</div>
-</div>
-    ${() => (doShowCheckConsentError.value ? $checkConsentError : "")}
-    <button @click="${onClickConsent}">Weiter</button>
-</div>
 `;
