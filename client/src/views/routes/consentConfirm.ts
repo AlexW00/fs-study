8e308e636fba2fde66578fcac3b87595c40714a1
Alexander Weichart <55558407+AlexW00@users.noreply.github.com>
2023-03-20 22:42:21 +0100

    back

--- client/src/views/routes/consentConfirm.ts
+++ client/src/views/routes/consentConfirm.ts
@@ -1,17 +1,53 @@
-import { html } from "@arrow-js/core";
+import { html, reactive } from "@arrow-js/core";
 import { SocketEvent } from "../../../../shared/SocketEvent";
 import { setDidGiveConsent } from "../../classes/State";
 import { SocketManager } from "../../Socket";
+
+const didCheckConsent = reactive({
+	value: false,
+});
+
+const doShowCheckConsentError = reactive({
+	value: false,
+});
 
 const onConsent = () => {
     setDidGiveConsent(true);
     SocketManager.getInstance().emit(SocketEvent.SendGiveConsent, undefined);
 }
 
-export const $consentConfirm = html`
-	<div>
-		<h1>Erklärungddd der Studie</h1>
-		
-		<button @click="${onConsent}">Verstanden</button>
+
+const onCheckConsent = () => {
+	didCheckConsent.value = !didCheckConsent.value;
+	if (didCheckConsent.value) {
+		doShowCheckConsentError.value = false;
+	}
+};
+
+const onClickConsent = () => {
+	console.log("onClickConsent");
+	if (!didCheckConsent.value) {
+		doShowCheckConsentError.value = true;
+	} else {
+		doShowCheckConsentError.value = false;
+		onConsent();
+	}
+};
+
+const $checkConsentError = html`
+	<div id="check-consent-error">
+		Bitte bestätigen Sie, dass Sie die Einverständniserklärung zur Aufklärung
+		über die Teilnahme gelesen habe^n.
 	</div>
+`;
+
+export const $consentConfirm = html`
+    <div class="consent-check" >
+        <input id="consent-checkbox" type="checkbox" name="Einverständniserklärung" @input="${onCheckConsent} value="${didCheckConsent.value}"/>
+        <label for="consent-checkbox">Ich habe die Einverständniserklärung zur Aufklärung über die Teilnahme sorgfältig durchgelesen und stimme dieser zu.</label>
+    </div>
+    </div>
+        ${() => (doShowCheckConsentError.value ? $checkConsentError : "")}
+        <button @click="${onClickConsent}">Verstanden</button>
+    /div>
 `;
